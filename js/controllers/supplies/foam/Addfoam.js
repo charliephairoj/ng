3c60f0013d210f675b43224c0cc2d58b65c0d9d1
Charlie Phairojmahakij <charliephairoj@gmail.com>
2013-03-20 13:42:13 +0700

    Added colors to foam and allow decimals for width and depth in foam

--- js/controllers/supplies/foam/Addfoam.js
+++ js/controllers/supplies/foam/Addfoam.js
@@ -3,18 +3,12 @@
 function AddFoamCtrl($scope, Foam, Supplier, $location, Poller){
     
     $scope.supplierList = Supplier.query();
+    $scope.foam = new Foam();
     //Methods
     
     //Add Lumber
-    $scope.save = function(){
-        //declare vars
-        var foam = new Foam();
-        //set properties
-        angular.copy($scope.foam, foam)
-        foam.foamType = $scope.foam.type;
-        foam.supplierID = $scope.foam.supplier.id;
-        
-        foam.$save(function(){
+    $scope.save = function(){       
+        $scope.foam.$save(function(){
             $location.path('/foam');
         });
     };
