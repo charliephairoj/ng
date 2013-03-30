966227b525a7922356610c0e22e552b62ba58c2e
Charlie Phairojmahakij <charliephairoj@gmail.com>
2013-03-30 13:33:26 +0700

    Add ability to save an updated Acknowledgement

--- js/controllers/orders/acknowledgements/details.js
+++ js/controllers/orders/acknowledgements/details.js
@@ -7,18 +7,22 @@
  * Acknowledgement Area
  */
 
-function AcknowledgementDetailsCtrl($scope, Acknowledgement, $routeParams){
+function AcknowledgementDetailsCtrl($scope, Acknowledgement, $routeParams, Notification){
     $scope.showCal = false;
-    $scope.acknowledgement = Acknowledgement.get({'id':$routeParams.id}, function(response){
-       console.log(response); 
-    });
+    $scope.acknowledgement = Acknowledgement.get({'id':$routeParams.id});
     
     $scope.updateDeliveryDate = function(){
         $scope.showCal = false;
-        
-    }
+    };
+    
+    $scope.save = function(){
+        Notification.display('Saving Acknowledgement...', false);
+        $scope.acknowledgement.$save(function(){
+            Notification.display('Acknowledgement '+$scope.acknowledgement.id+' Saved');
+        });
+    };
 }
 
-AcknowledgementDetailsCtrl.$inject = ['$scope', 'Acknowledgement', '$routeParams'];
+AcknowledgementDetailsCtrl.$inject = ['$scope', 'Acknowledgement', '$routeParams', 'Notification'];
 
 
