2df0a19b9035ba8ceaa13239c8c6a54a934798c5
Charlie Phairojmahakij <charliephairoj@gmail.com>
2013-04-20 13:32:10 +0700

    Change height to auto for grid in transaction

--- scripts/controllers/accounting/transaction/add.js
+++ scripts/controllers/accounting/transaction/add.js
@@ -1,14 +1,15 @@
 'use strict';
 
 angular.module('employeeApp')
-  .controller('AccountingTransactionAddCtrl', ['$scope', 'Transaction', 'Notification', 
-  function ($scope, Transaction, Notification) {
+  .controller('AccountingTransactionAddCtrl', ['$scope', 'Transaction', 'Notification', '$location',
+  function ($scope, Transaction, Notification, $location) {
     $scope.transaction = new Transaction();
     
     $scope.save = function(){
         Notification.display('Saving Transaction...', false);
         $scope.transaction.$save(function(){
             Notification.display('Transaction Saved');
+            $location.path('/accounting/transaction');
         });
     }
   }]);
