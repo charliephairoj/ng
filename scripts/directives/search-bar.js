d60a751972d30676044d60566d45105313514582
Charlie Phairojmahakij <charliephairoj@gmail.com>
2013-04-25 12:02:53 +0700

    Enable hiding and showing of search bar

--- scripts/directives/search-bar.js
+++ scripts/directives/search-bar.js
@@ -9,20 +9,37 @@ angular.module('employeeApp')
           element.addClass('search-bar');
           element.append(angular.element('<input size="40" placeholder="Search" ng-model="query" />'));
           
+          /*
+           * The handler will detect if ctrl/cmd+F 
+           * is pressed on the keyboard
+           * 
+           * If press occurs, we disable the default action
+           * perform state specific actions
+           */
           function searchHandler(evt){
-              console.log(evt);
-              console.log(evt);
               if((evt.which == "70" && (evt.metaKey || evt.ctrlKey))){
-                  console.log("STRG+F");
                   evt.preventDefault();
-                  $("#searchbox").slideDown(110);
-                  $('#search').focus();
+                  
+                  //Determine
+                  if(element.hasClass('focus')){
+                      //Hide object and blur
+                      element.blur();
+                      element.removeClass('focus');
+                  }else{
+                      //Show and focus
+                      element.focus();
+                      element.addClass('focus');   
+                  }
+                  
               }
           }
           
-          $(window).bind('keydown', searchHandler);
+          //Bind an action 
+          $(window).on('keydown', searchHandler);
+          
+          //Unbind when leaving the Page
           scope.$on('$destroy', function(){
-              $(window).unbind('keydown', searchHandler); 
+              $(window).off('keydown', searchHandler); 
           });
       }
     };
