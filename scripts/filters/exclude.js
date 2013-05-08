fc6b93cb9518ae4478dae17abca77464b2de5650
Charlie Phairojmahakij <charliephairoj@gmail.com>
2013-05-08 19:31:01 +0700

    Change exclude to exclude by key

--- scripts/filters/exclude.js
+++ scripts/filters/exclude.js
@@ -3,20 +3,17 @@
 angular.module('employeeApp.filters')
     .filter('exclude', [function () {
         //function to compare
-          return function(array, targetArray) {
+          return function(array, key, value) {
             if (!(array instanceof Array)) {return []};
-            if (!(targetArray instanceof Array)) {targetArray = [targetArray]}
            
-            var filtered = [],
-                i;
-            angular.forEach(array, function(item){
-                for(i=0; i<targetArray.length; i++){
-                    if(!angular.equals(item, targetArray[i]) && i===targetArray.length-1){
-                        filtered.push(item);
-                        break;
-                    }  
+            var filtered = [];
+            
+            for(var i in array){
+                if(!angular.equal(array[i][key], value)){
+                    filtered.push(array[i]);
                 }
-            });
+            }
+            
             return filtered;
           };
     }]);
