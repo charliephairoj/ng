091725215a622c00e36cfe7d603382a15219fe0f
Charlie Phairojmahakij <charliephairoj@gmail.com>
2013-08-06 10:07:02 +0700

    Added row highlighting

--- scripts/controllers/supply/view.js
+++ scripts/controllers/supply/view.js
@@ -14,20 +14,22 @@ angular.module('employeeApp')
      	
      	//Search Mechanism
      	$scope.$watch('query', function(query){
-     		$scope.data = $filter('filter')(supplyList, query);
+     		$scope.data = $filter('limitTo')($filter('filter')(supplyList, query), 50);
      	});
      	
      	//Grid Options
      	$scope.gridOptions = {
      		data: 'data',
+     		rowHeight: 150,
      		columnDefs: [{field: 'id', displayName: 'ID', width: '75px'},
      					 {field: 'image.url',
+     					  width: '150px',
      					  displayName: 'Example',
      					  cellClass: 'image',
      					  cellTemplate: '<img ng-src="{{row.getProperty(col.field)}}"/>'},
      					 {field: 'supplier.name', displayName: 'Supplier'},
      					 {field: 'description', displayName: 'Description'},
-     					 {field: 'type', displayName: 'Type'},
+     					 {field: 'type', displayName: 'Type', width: '75px'},
      					 {field: 'quantity', displayName: 'Quantity in Stock'}]
      	};
      	
---
+++ scripts/directives/ngGrid.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/*
+angular.module('employeeApp')
+  	.directive('ngGrid', [function () {
+  		var data,
+  			options;
+    	return {
+      		template: '<div></div>',
+      		restrict: 'A',
+      		scope: {
+      			options: '=ngGrid',
+      			filter: '=ngGridFilter'
+      		}
+      		link: function postLink(scope, element, attrs) {
+      			
+        		
+      		}
+    	};
+  	}]);
+*/
