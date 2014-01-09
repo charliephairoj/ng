4f49a120356989da053957be0ca7822f7d0a740e
= <=>
2014-01-09 15:05:17 +0700

    Fix lint

--- scripts/controllers/order/acknowledgement/create.js
+++ scripts/controllers/order/acknowledgement/create.js
@@ -59,7 +59,6 @@ function ($scope, Acknowledgement, Customer, $filter, Notification) {
             }
         } catch (e) {
             Notification.display(e.message, false);
-			throw new Error(e);
         }
     };
     
@@ -76,19 +75,23 @@ function ($scope, Acknowledgement, Customer, $filter, Notification) {
          * The property has already been added
          */
         if (!$scope.ack.customer) {
-            throw new TypeError("Customer is not an object");
+            throw new TypeError("Please add a customer.");
         } else {
             if (!$scope.ack.customer.hasOwnProperty('id')) {
                 throw new ReferenceError("Missin customer ID");
             }
         }
+        
+        //Validate ordered Items
         if (!$scope.ack.items) {
             throw new TypeError("Products is not an array");
         } else {
+			//Verifies that there are items ordered
             if ($scope.ack.items.length <= 0) {
                 throw new RangeError("No products added to the order");
             } else {
                 for (var i=0; i<$scope.ack.items.length; i++) {
+					var item = $scope.ack.items[i];
                     /*
                      * Check that there is a quantity 
                      * for each piece of product
@@ -96,6 +99,10 @@ function ($scope, Acknowledgement, Customer, $filter, Notification) {
                     if (!$scope.ack.items[i].hasOwnProperty('quantity') || !$scope.ack.items[i].quantity) {
                         throw new RangeError("Expecting a quantity of at least 1 for "+$scope.ack.items[i].description);
                     }
+                    
+                    /*
+                     * Validates that every item has a price
+                     */
                     if (!$scope.ack.items[i].hasOwnProperty('has_price')) {
                         //throw new ReferenceError("Product missing 'has_price' attribute");
                     } else {
@@ -103,16 +110,30 @@ function ($scope, Acknowledgement, Customer, $filter, Notification) {
                             //throw new TypeError("Product missing price");
                         }
                     }
+                    
+                    /*
+                     * Validates custom items
+                     */
+                    if (!item.hasOwnProperty('id')) {
+						if (!item.is_custom) {
+							throw new TypeError("Item without id is not custom. Please contact an Administrator.");
+						}
+                    }
                 }
             }
         }
         
-        if ($scope.ack.vat === undefined || $scope.ack.vat === null) {
-            throw new TypeError("Vat is not a number");
-        }
+        //Validate Delivery Date
         if (!$scope.ack.delivery_date) {
-            throw new TypeError("Delivery Date is not a valid Date type");
+            throw new TypeError("Please select a preliminary delivery date.");
         }
+        
+        //Validate vat
+        if ($scope.ack.vat === undefined || $scope.ack.vat === null) {
+            throw new TypeError("Please set the vat.");
+        }
+        
+        //Validate purchase order number
         if (!$scope.ack.po_id) {
             throw new TypeError("PO# is not defined");
         }
