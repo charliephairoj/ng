e972d59e2c4b1272976673ad8d0eadc117e64ceb
= <=>
2014-01-17 11:52:23 +0700

    fixed units for add suppy

--- scripts/directives/add-supply.js
+++ scripts/directives/add-supply.js
@@ -28,7 +28,7 @@ angular.module('employeeApp.directives')
 			};
 			
 			scope.supply = new Supply();
-			scope.supply.units = 'piece';
+			scope.supply.units = 'pc';
 			scope.suppliers = Supplier.query({limit:0});
 			
 			
